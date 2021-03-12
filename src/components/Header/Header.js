bd92ca4e44e0709b5d6281de2a164da1cc39fcea
MarufHimalay <m.u.maruf1999@gmail.com>
2021-03-12 20:48:38 +0600

    search box added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    
+
     const headerStyle = {
-        backgroundImage: `url(https://cdn.ttgtmedia.com/visuals/ComputerWeekly/Hero%20Images/sport-football-1-adobe.jpeg)`, 
-        backgroundPosition: 'center', backgroundSize:'80%',
-        height:'200px',
+        backgroundImage: `url(https://cdn.ttgtmedia.com/visuals/ComputerWeekly/Hero%20Images/sport-football-1-adobe.jpeg)`,
+        backgroundPosition: 'center', backgroundSize: '80%',
+        // height: '200px',
         paddingTop: '80px',
         paddingBottom: '100px'
     }
 
-    const navStyle = { 
-        backgroundColor:'black',
-        padding: '5px', 
-        color: 'black', 
-        opacity: '0.6', 
-        fontWeight: 'bold' }
+    const navStyle = {
+        backgroundColor: 'black',
+        padding: '5px',
+        color: 'black',
+        opacity: '0.6',
+        fontWeight: 'bold'
+    }
 
     const linkStyle = { textDecoration: 'none', color: 'white' }
 
+    const [search, setSearch] =useState('@@');
+    const [player, setPlayer] = useState([]);
+    const handleChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+    useEffect(() => {
+        fetch(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${search}`)
+        .then( res => res.json())
+        .then(data => {
+                setPlayer(data.player)
+        })
+    }, [search])
+    console.log(search);
     return (
         <div style={headerStyle}>
             <nav style={navStyle} className="text-center">
@@ -27,6 +44,13 @@ const Header = () => {
                     <Link style={linkStyle} to="/">Football Tracker</Link>
                 </h1>
             </nav>
+            <div style={{width: '50%',margin: 'auto'}} className="input-group mb-3 pt-3">
+                <span className="input-group-text" id="inputGroup-sizing-default">search by player</span>
+                <input onChange={handleChange} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+            </div>
+             {
+                    player?.map( pl => <h2 className="text-center">{pl?.strPlayer}</h2>)
+            } 
         </div>
     );
 };
